78c0988dfc2264f9677388fb3305fa2b97aec916
developervilchez <developervilchez@gmail.com>
2019-05-14 22:24:40 -0500

    identando y personalizand viewSantiago

--- src/main.js
+++ src/main.js
@@ -144,28 +144,83 @@ const viewPaloma = () => {
 
 
 const viewSantiago = () => {
- return ` <nav class="container navbar" id="navbar-container-psico-view-santiago">
- <div class="nav-wrapper">
-   <a href="#" class="brand-logo logo"><img class="img-logo" alt="logo-sesion" src="images/sesion7_200x96.png"/></a>
-   <ul id="nav-mobile" class="right list-items">
-     <li class="item"><a class="font-black" href="#how-work">¿Cómo Funciona?</a></li>
-     <li class="item"><a class="font-black" href="#team">El equipo</a></li>
-     <li class="item"><a class="font-black" href="#faq">FAQ</a></li>
-     <li class="item"><a  id="start-now" class="font-black" href="#sesion">Empezar ahora</a></li>
-   </ul>
- </div>
-</nav>
- <div class="section">
- <div class="row">
-   <div class="col s12">
-     <div class="icon-block">
-       <h2 class="center brown-text"><img class="circle responsive-img profile" src="images/santiago.png"/></h2>
-       <h5 class="center">Santiago Joseph</h5>
-       <p class="light">Lic. En Psicología Clínica y Mg. en Estudios Teóricos en Psicoanálisis</p>
+ return `
+ <section>
+   <nav class="container navbar" id="navbar-container-psico-view-paloma">
+     <div class="nav-wrapper">
+       <a href="#" class="brand-logo logo"><img class="img-logo" alt="logo-sesion" src="images/sesion7_200x96.png"/></a>
+       <ul id="nav-mobile" class="right list-items">
+         <li class="item"><a class="font-black" href="#how-work">¿Cómo Funciona?</a></li>
+         <li class="item"><a class="font-black" href="#team">El equipo</a></li>
+         <li class="item"><a class="font-black" href="#faq">FAQ</a></li>
+         <li class="item"><a  id="start-now" class="font-black" href="#sesion">Empezar ahora</a></li>
+       </ul>
      </div>
+    </nav>
+    <section class="row">
+     <div class="col s12">
+       <div class="icon-block">
+         <h2 class="center brown-text"><img class="circle responsive-img profile" src="images/santiago.png"/></h2>
+         <h5 class="center">Santiago Joseph</h5>
+         <p class="light">Lic. En Psicología Clínica y Mg. en Estudios Teóricos en Psicoanálisis</p>
+       </div>
+       <div>
+         <h3>Bio</h3>
+         <p>Santiago es ...</p>
+         <h3>Intereses</h3>
+       </div>
+       <div class="icon-block">
+         <h3>Especialista en: </h3> 
+         <ul>
+          <li>- Psicoterapeuta de adolescentes y adultos.</li>
+          <li>- Experiencia en estrés laboral y crisis vitales.</li>
+         </ul>
+         <!-- Modal Trigger -->
+         <a class="waves-effect waves-light btn modal-trigger" href="#modal1" id="contacto-joseph">Contactar</a>
+         <!-- Modal Structure -->
+         <div id="modal1" class="modal">
+           <div class="modal-content">
+             <h4>Información Previa</h4>
+             <div class="row">
+               <form class="col s12">
+                 <div class="input-field col s12">
+                   <i class="material-icons prefix">account_circle</i>
+                   <input id="icon_prefix" type="text" class="validate">
+                   <label for="icon_prefix">Nombres y Apellidos</label>
+                 </div>
+                 <div class="input-field col s12">
+                   <i class="material-icons prefix">phone_iphone</i>
+                   <input id="icon_telephone" type="tel" class="validate">
+                   <label for="icon_telephone">Celular</label>
+                 </div>
+                 <div class="input-field col s12">
+                   <i class="material-icons prefix">email</i>
+                   <input id="icon_email" type="email" class="validate">
+                   <label for="icon_email">Email</label>
+                 </div>
+                 <div class="input-field col s12">
+                   <h4>¿Cuántas sesiones deseas tomar?</h4>
+                   <input type="radio" name="planes" value="1">Una sesión<br>
+                   <input type="radio" name="planes" value="4">Paquete de 4 sesiones<br>
+                   <input type="radio" name="planes" value="0">Todavía no lo decido
+                 </div>
+                 <div class="input-field col s12">
+                   <h4>¿Qué te animó a iniciar una sesión?</h4>
+                   <i class="material-icons prefix">mode_edit</i>
+                   <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
+                   <label for="icon_prefix2">Cuéntame</label>
+                 </div>
+               </form> 
+             </div>
+           </div>
+           <div class="modal-footer">
+             <a href="#!" class="modal-close waves-effect waves-green btn-flat" id="contacta-joseph-wasap">Inicia Sesión</a>
+           </div>
+         </div>  
+       </div>
      </div>
-     </div>
-     </div>`
+   </section>
+  </section>`
 }
 
 const viewAmelie = () => {
