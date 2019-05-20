9e984d49227d1646ebb8d58207ee560ceb51181f
developervilchez <developervilchez@gmail.com>
2019-05-20 15:18:13 -0500

    modifica boton de contacto

--- src/main.js
+++ src/main.js
@@ -178,11 +178,6 @@ const viewSantiago = () => {
          <h5 class="center">Santiago Joseph</h5>
          <p class="light">Lic. En Psicología Clínica y Mg. en Estudios Teóricos en Psicoanálisis</p>
        </div>
-       <div>
-         <h3>Bio</h3>
-         <p>Santiago es ...</p>
-         <h3>Intereses</h3>
-       </div>
        <div class="icon-profile">
          <h3>Especialista en: </h3> 
          <ul>
@@ -213,22 +208,32 @@ const viewSantiago = () => {
                    <label for="icon_email">Email</label>
                  </div>
                  <div class="input-field col s12">
-                   <h4>¿Cuántas sesiones deseas tomar?</h4>
-                   <input type="radio" name="planes" value="1">Una sesión<br>
-                   <input type="radio" name="planes" value="4">Paquete de 4 sesiones<br>
-                   <input type="radio" name="planes" value="0">Todavía no lo decido
+                   <h5>¿Cuántas sesiones deseas tomar?</h5>
+                   <div class="container-inputs">
+                   <label>
+                    <input name="planes" type="radio" value="1" />
+                    <span>1 sesión</span>
+                   </label>
+                   <label>
+                    <input name="planes" type="radio" value="4" />
+                    <span>4 sesiones</span>
+                   </label>
+                   <label>
+                    <input name="planes" type="radio" value="0" />
+                    <span>Todavía no lo sé</span>
+                   </label>
+                   <div>
                  </div>
                  <div class="input-field col s12">
-                   <h4>¿Qué te animó a iniciar una sesión?</h4>
+                   <h6>¿Qué te animó a iniciar Sesión?</h6>
                    <i class="material-icons prefix">mode_edit</i>
                    <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
-                   <label for="icon_prefix2">Cuéntame</label>
                  </div>
                </form> 
              </div>
            </div>
            <div class="modal-footer">
-             <a href="#!" class="modal-close waves-effect waves-green btn-flat" id="contacta-santiago-wasap">Inicia Sesión</a>
+             <a href="#!" class="modal-close waves-effect waves-green btn-flat button-secondary" id="contacta-santiago-wasap">Inicia Sesión</a>
            </div>
          </div>  
        </div>
