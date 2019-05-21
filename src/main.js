49bdee19ac5501f687cf6d9b5d7a8ebc6ae40826
Gabriela Segura <gabriela.segura04@gmail.com>
2019-05-20 23:20:57 -0500

    Añade estilos para perfil de psicoterapeuta

--- src/main.js
+++ src/main.js
@@ -93,67 +93,90 @@ const viewPaloma = () => {
          <li><a href="#" class="brand-logo logo info-logo"><img class="img-logo" alt="logo-sesion" src="images/sesion7_200x96.png"/></a></li>
         </ul>
       </nav>
-    </div> 
+    </div>
+
+    <section class="container section">
+    <div class="row">
+      <div class="col s12">
+        <h2 class="section-title">Perfil psicoterapeuta</h2>
+      </div>
+    </div>
+
     <section class="row">
       <div class="col s12">
-        <div class="icon-profile">
-          <h2 class="center brown-text"><img class="circle responsive-img profile" src="images/paloma.png"/></h2>
-          <h5 class="center">Paloma Reaño</h5>
-          <p class="light">Lic. En Psicología Clínica y Mg. en Estudios Teóricos en Psicoanálisis</p>
-        </div>
-        <div>
-          <h3>Bio</h3>
-          <p>Paloma es ...</p>
-          <h3>Intereses</h3>
-        </div>
-        <div class="icon-profile">
-          <h3>Especialista en: </h3> 
-          <ul>
-           <li>- Psicoterapeuta de niños, adolescentes y adultos.</li>
-           <li>- Experiencia en trabajo vincular y asesoría de crianza a padres.</li>
-          </ul>
-          <!-- Modal Trigger -->
-          <a class="waves-effect waves-light btn modal-trigger" href="#modal1" id="contacto-paloma">Contactar</a>
-          <!-- Modal Structure -->
-          <div id="modal1" class="modal">
-            <div class="modal-content">
-              <h4>Información Previa</h4>
-              <div class="row">
-                <form class="col s12">
-                  <div class="input-field col s12">
-                    <i class="material-icons prefix">account_circle</i>
-                    <input id="icon_prefix" type="text" class="validate">
-                    <label for="icon_prefix">Nombres y Apellidos</label>
-                  </div>
-                  <div class="input-field col s12">
-                    <i class="material-icons prefix">phone_iphone</i>
-                    <input id="icon_telephone" type="tel" class="validate">
-                    <label for="icon_telephone">Celular</label>
-                  </div>
-                  <div class="input-field col s12">
-                    <i class="material-icons prefix">email</i>
-                    <input id="icon_email" type="email" class="validate">
-                    <label for="icon_email">Email</label>
-                  </div>
-                  <div class="input-field col s12">
-                    <h4>¿Cuántas sesiones deseas tomar?</h4>
-                    <input type="radio" name="planes" value="1">Una sesión<br>
-                    <input type="radio" name="planes" value="4">Paquete de 4 sesiones<br>
-                    <input type="radio" name="planes" value="0">Todavía no lo decido
-                  </div>
-                  <div class="input-field col s12">
-                    <h4>¿Qué te animó a iniciar una sesión?</h4>
-                    <i class="material-icons prefix">mode_edit</i>
-                    <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
-                    <label for="icon_prefix2">Cuéntame</label>
-                  </div>
-                </form> 
+        <div class="card">
+          <div class="icon-profile">
+            <div class="row icon-profile-wrapper">
+              <div class="col s12 l4">
+                <h2 class="center brown-text"><img class="circle responsive-img profile" src="images/paloma.png"/></h2>
+              </div>
+              <div class="col s12 l8">
+                <h5 class="center">Paloma Reaño</h5>
+                <p class="light">Lic. En Psicología Clínica de la Pontificia Universidad Católica del Perú y Mg. En Estudios Teóricos en Psicoanálisis de la PUCP. </p>
+                </div>
+            </div>
+          </div>
+          <div class="info-profile container">
+            <div class="row">
+              <div class="col s10 offset-s1 m12">
+                <h5>Bio</h5>
+                <p>Paloma es ...</p>
+                <h5>Intereses</h5>
+
+                <h5>Especialista en: </h5> 
+                <ul class="bullet-list">
+                  <li>Psicoterapeuta de niños, adolescentes y adultos.</li>
+                  <li>Experiencia en trabajo vincular y asesoría de crianza a padres.</li>
+                </ul>
+                <!-- Modal Trigger -->
+                <a class="waves-effect waves-light button-primary modal-trigger" href="#modal1" id="contacto-paloma">Contactar</a>
               </div>
             </div>
-            <div class="modal-footer">
-              <a href="#!" class="modal-close waves-effect waves-green btn-flat" id="contacta-paloma-wasap">Inicia Sesión</a>
+
+            <!-- Modal Structure -->
+            <div id="modal1" class="modal">
+              <div class="modal-content">
+                <h4>Información Previa</h4>
+                <div class="row">
+                  <form class="col s12">
+                    <div class="input-field col s12">
+                      <i class="material-icons prefix">account_circle</i>
+                      <input id="icon_prefix" type="text" class="validate">
+                      <label for="icon_prefix">Nombres y Apellidos</label>
+                    </div>
+                    <div class="input-field col s12">
+                      <i class="material-icons prefix">phone_iphone</i>
+                      <input id="icon_telephone" type="tel" class="validate">
+                      <label for="icon_telephone">Celular</label>
+                    </div>
+                    <div class="input-field col s12">
+                      <i class="material-icons prefix">email</i>
+                      <input id="icon_email" type="email" class="validate">
+                      <label for="icon_email">Email</label>
+                    </div>
+                    <div class="input-field col s12">
+                      <h4>¿Cuántas sesiones deseas tomar?</h4>
+                      <input type="radio" name="planes" value="1">Una sesión<br>
+                      <input type="radio" name="planes" value="4">Paquete de 4 sesiones<br>
+                      <input type="radio" name="planes" value="0">Todavía no lo decido
+                    </div>
+                    <div class="input-field col s12">
+                      <h4>¿Qué te animó a iniciar una sesión?</h4>
+                      <i class="material-icons prefix">mode_edit</i>
+                      <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
+                      <label for="icon_prefix2">Cuéntame</label>
+                    </div>
+                  </form> 
+                </div>
+              </div>
+              <div class="modal-footer">
+                <a href="#!" class="modal-close waves-effect waves-green btn-flat" id="contacta-paloma-wasap">Inicia Sesión</a>
+              </div>
             </div>
-          </div>  
+            
+            <!--End info-profile-->
+          </div>
+          
         </div>
       </div>
     </section>
