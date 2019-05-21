5b75d0a5045c546800daa62500497b1bc6c711e0
developervilchez <developervilchez@gmail.com>
2019-05-21 00:24:38 -0500

    modifica vista detalle y modal

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ root.addEventListener('click', (event) => {
   if (event.target.id === 'start-now' || event.target.id === 'start-now-mobile' || event.target.id === 'start-now-info' || event.target.id === 'choose-psico') {
     event.currentTarget.innerHTML = HeaderProfile() + SectionPsicoProfile(data)
   };
-  if(event.target.localName === "a"){
-    console.log(data[event.target.id])
-  }
+   if (data.hasOwnProperty(event.target.id)){
+    event.currentTarget.innerHTML = HeaderDetailProfile() + SectionDetailPsicoProfile(data[event.target.id]);
+    $('.modal').modal();
+   }
 })
---
+++ src/views/section-detail-psico-profile.js
@@ -0,0 +1,21 @@
+const SectionDetailPsicoProfile = (psico) => {
+  console.log(psico)
+  return `
+    <section class="row">
+      <div class="col s12">
+        <div class="icon-profile">
+          <h2 class="center brown-text"><img class="circle responsive-img profile" src=${psico.imagen} /></h2>
+          <h5 class="center">${psico.nombre}</h5>
+          <p class="light">${psico.formacion}</p>
+        </div>
+        <div class="icon-profile">
+          <h3>Especialista en: </h3> 
+          <ul>
+            ${listaEspecialidadProfile(psico.especilidades)}
+          </ul>
+          <!-- Modal Trigger -->
+          <a class="waves-effect waves-light btn modal-trigger" href="#modal1" id=${psico.id}>Contactar</a>
+           ${ModalDetails(psico.id, psico.celular)}
+        </div>
+    </section>`
+}
