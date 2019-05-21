c4ce40bac01e319df448d195eab12a78fe76e306
developervilchez <developervilchez@gmail.com>
2019-05-21 04:07:38 -0500

    modifica el link del logo hacia la vista principal

--- src/main.js
+++ src/main.js
@@ -8,9 +8,11 @@ root.addEventListener('click', (event) => {
   if (event.target.id === 'start-now' || event.target.id === 'start-now-mobile' || event.target.id === 'start-now-info' || event.target.id === 'choose-psico') {
     event.currentTarget.innerHTML = `<div id="vista-profile">${HeaderProfile()}${SectionPsicoProfile(data)}<div/>`;
   };
+
   if (event.target.className === 'img-logo') {
     window.location = 'index.html';
   };
+
    if (data.hasOwnProperty(event.target.id)){
     event.currentTarget.innerHTML = HeaderProfile() + SectionDetailPsicoProfile(data[event.target.id]);
     $('.modal').modal();
