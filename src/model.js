7f1b174505760294b3879c98207a0f1fdbdaaa7c
developervilchez <developervilchez@gmail.com>
2019-05-20 21:11:27 -0500

    estructurando la data por especialista

---
+++ src/model.js
@@ -0,0 +1,23 @@
+const data = {
+  Paloma : {
+   id : 'psico-1',
+   nombre : 'Paloma Reaño',
+   formacion : 'Psicóloga clínica y Mg. en Estudios Teóricos en Psicoanálisis',
+   especilidades : ['Psicoterapeuta de niños, adolescentes y adultos.', 'Experiencia en trabajo vincular y asesoría de crianza a padres.'],
+   celular : '51966422821', 
+  },
+  Santiago : {
+   id : 'psico-2',
+   nombre : 'Santiago Joseph',
+   formacion :'Psicólogo Clínico y Mg. En Estudios Teóricos en Psicoanálisis',
+   especilidades : ['Psicoterapeuta de adolescentes y adultos.', 'Experiencia en estrés laboral y crisis vitales.'],
+   celular : '51984119144'
+  },
+  Amelie :{
+    id: 'psico-3',
+    nombre: 'Amélie Caillaux',
+    formacion : 'Psicóloga y terapeuta psicoanalítica',
+    especilidades : ['Psicoterapeuta psicoanalítica egresada del Instituto Inter-Cambio.','Experiencia en asesoría pedagógica y orientación a padres de familia en estilos de crianza.'],
+    celular : '51987959553'
+  }
+}
