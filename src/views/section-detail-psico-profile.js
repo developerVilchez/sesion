0646193db587167b1da1a520eee732fedef273cf
Gabriela Segura <gabriela.segura04@gmail.com>
2019-05-21 01:13:12 -0500

    Añade clases a estructura perfil psicoterapeuta

--- src/views/section-detail-psico-profile.js
+++ src/views/section-detail-psico-profile.js
@@ -1,20 +1,35 @@
 const SectionDetailPsicoProfile = (psico) => {
   return `
-    <section class="row">
-      <div class="col s12">
-        <div class="icon-profile">
-          <h2 class="center brown-text"><img class="circle responsive-img profile" src=${psico.imagen} /></h2>
-          <h5 class="center">${psico.nombre}</h5>
-          <p class="light">${psico.formacion}</p>
+    <section class="container">
+      <div class="row">
+        <div class="col s12">
+          <div class="card">
+            <div class="icon-profile">
+              <div class="row icon-profile-wrapper">
+                <div class="col s12 l4">
+                  <h2 class="center brown-text"><img class="circle responsive-img profile" src=${psico.imagen} /></h2>
+                </div>
+                <div class="col s12 l8">
+                  <h5 class="center">${psico.nombre}</h5>
+                  <p class="light">${psico.formacion}</p>
+                </div>
+              </div>
+            </div>
+            <div class="info-profile container">
+              <div class="row">
+                <div class="col s10 offset-s1 m12">
+                  <h5>Especialista en: </h5> 
+                  <ul>
+                    ${listaEspecialidadProfile(psico.especilidades)}
+                  </ul>
+                  <!-- Modal Trigger -->
+                  <a class="waves-effect waves-light button-primary modal-trigger" href="#modal1" id=${psico.id}>Contactar</a>
+                  ${ModalDetails(psico.id, psico.celular)}
+                </div>
+              </div>
+            </div>
+          </div> 
         </div>
-        <div class="icon-profile">
-          <h3>Especialista en: </h3> 
-          <ul>
-            ${listaEspecialidadProfile(psico.especilidades)}
-          </ul>
-          <!-- Modal Trigger -->
-          <a class="waves-effect waves-light btn modal-trigger" href="#modal1" id=${psico.id}>Contactar</a>
-           ${ModalDetails(psico.id, psico.celular)}
-        </div>
+      </div>
     </section>`
 }
