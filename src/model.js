2a5171e1e11f110474eab8a00f9dba67925eac6f
developervilchez <developervilchez@gmail.com>
2019-05-29 07:14:52 -0500

    modifica perfil de paloma

--- src/model.js
+++ src/model.js
@@ -2,12 +2,13 @@ const data = {
   Paloma : {
    id : 'psico-1',
    nombre : 'Paloma Reaño',
-   imagen : 'src/images/paloma.png',
+   imagen : 'src/images/paloma.jpg',
    formacion : 'Psicóloga clínica y Mg. en Estudios Teóricos en Psicoanálisis',
-   especilidades : ['Psicoterapeuta de niños, adolescentes y adultos.', 'Experiencia en trabajo vincular y asesoría de crianza a padres.'],
+   especilidades : ['Psicoterapeuta emocional con niños, adolescentes y adultos.', 'Experiencia en trabajo vincular.' , 'Asesoría en crianza para padres.', 'Psicoterapia breve o focal.'],
    celular : '51966422821', 
    perfil: ['psicoterapeuta'],
-   bio : ''
+   bio : 'Soy Licenciada en Psicología Clínica y Magister en Estudios Teóricos en Psicoanálisis de la PUCP. Tengo experiencia en el trabajo psicoterapéutico con niños, adolescentes y adultos, así como en el trabajo vincular e intervenciones comunitarias grupales. Utilizo el marco de la psicoterapia psicoanalítica y describiría mi estilo como cálido y dinámico',
+   percepcion: 'Considero que la psicoterapia online permite establecer el vínculo seguro y estable que un proceso emocional requiere, lo cual nos permite tratar los temas que tú desees abordar',
   },
   Santiago : {
    id : 'psico-2',
